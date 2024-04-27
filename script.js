96592d458149f44e6c40dda0f9901cb507b42f66
William Allen <williamallen933@gmail.com>
2024-04-27 14:29:24 -0400

    got rid of the for method incorporated const and if else statement

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let question = window.prompt("What window method am I using?")
+const question = window.prompt("What window method am I using?");
+const correctAnswer= "window.prompt"
+let userAnswer = window.alert(`Your answer: ${question}`);
+if (userAnswer = correctAnswer){
+    window.alert ("Correct!!!");
+}
+else { 
+    window.alert ("Incorrect. Please try again")
+}
